72077dc90b4fcc4fa8b114a7d7aa1b0e269ca747
sunyunli <651575315@qq.com>
2025-08-26 13:59:16 +0800

    2025年8月26日 增加cms判断逻辑 判断健康次数来对事件进行鉴别

--- data-web-process/src/main/java/com/ali/zn/data/task/CMS2ZnTSDBApplication.java
+++ data-web-process/src/main/java/com/ali/zn/data/task/CMS2ZnTSDBApplication.java
@@ -6,16 +6,23 @@ import lombok.extern.log4j.Log4j2;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.JedisSentinelPool;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Properties;
+import java.util.Set;
 
 @Log4j2
 public class CMS2ZnTSDBApplication {
+
+    public  static  String redisPrefix = "CMS::";
 
     public static void main(String[] args) {
         log.info(" kafka  算法结构并 实现数据推送--");
@@ -63,13 +70,25 @@ public class CMS2ZnTSDBApplication {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+        String masterName = "mymaster"; // 主节点名称
+        Set<String> sentinels = new HashSet<>();
+        sentinels.add("10.166.15.216:26379");
+        sentinels.add("10.166.15.217:26379");
+        sentinels.add("10.166.15.218:26379");
+        // 连接池配置
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(20);
+        poolConfig.setMaxIdle(5);
 
+        JedisSentinelPool sentinelPool = new JedisSentinelPool(masterName, sentinels, poolConfig,10000,"Zjhc@1234!",14);
+
+//        Jedis jedis = sentinelPool.getResource();
 
         while (true) {
             try {
                 ConsumerRecords<String, String> recordList = consumer.poll(1000);
                 Boolean needSleep = true;
-
+                Jedis jedis = sentinelPool.getResource();
                 for (ConsumerRecord<String, String> onerecord : recordList) {
                     needSleep = false;
 //                    System.out.println(onerecord.partition() + "   " + onerecord.offset() + "   " + );
@@ -78,8 +97,13 @@ public class CMS2ZnTSDBApplication {
                     try {
                         JSONObject jo = JSONObject.parseObject(onerecord.value().toString());
                         String time = jo.getString("recorded_time");
+
+                        JSONObject redisJo = new JSONObject();
+
+                        redisJo.put("recorded_time", time);
                         if (time.length() == 19) {
                             StringBuilder sqlbuilderback = new StringBuilder("insert into   ");
+
                             Double rotation_speed = jo.getDouble("rotation_speed");
                             Double gearbox_rms_1 = jo.getDouble("gearbox_rms_1");
                             Double gearbox_rms_2 = jo.getDouble("gearbox_rms_2");
@@ -102,13 +126,58 @@ public class CMS2ZnTSDBApplication {
                             Double mechanical_index1 = jo.getDouble("mechanical_index1");
                             Double mechanical_index2 = jo.getDouble("mechanical_index2");
                             Double mechanical_index3 = jo.getDouble("mechanical_index3");
+                            redisJo.put("operational_status", operational_status);
+                            redisJo.put("alert_location", alert_location);
+                            String event_operational_status = jo.getString("operational_status");
+                            String event_alert_location = jo.getString("alert_location");
 
+                            //2025年8月25日新增三个字段
+                            Double main_bearing_health_index1 = jo.getDouble("main_bearing_health_index1");
+                            Double main_bearing_health_index2 = jo.getDouble("main_bearing_health_index2");
+                            Double main_bearing_health_index3 = jo.getDouble("main_bearing_health_index3");
 
+                            redisJo.put("alarmtimes",0);
                             String device_id = jo.getString("device_id");
                             String tagName = "5211.CMS." + device_id;
+                            String redisKey = redisPrefix + tagName;
+                            // 遇到不健康的则更新 状态
+                            String  alarmStatus= jedis.get(redisKey);
+                            if (!operational_status.equals("健康")) {
+                                if(alarmStatus==null) {
+                                    redisJo.put("alarmtimes",1);
+                                    jedis.set(redisKey,  redisJo.toJSONString());
+                                }else{
+                                    // 更新redis 数字
+                                    JSONObject jo1 = JSONObject.parseObject(alarmStatus);
+                                    Integer alarmNums=jo1.getInteger("alarmtimes")+1;
+                                    redisJo.put("alarmtimes",alarmNums);
+                                    jedis.set(redisKey, redisJo.toString());
+                                    if(alarmNums>=3){
+                                        // 更新后续值状态
+                                        event_operational_status =operational_status;
+                                        event_alert_location = alert_location;
+                                    }
+                                }
+                            }else{
+                                JSONObject jo1 = JSONObject.parseObject(alarmStatus);
+                                // 当不满足 连续三次式需要清空redis 里面的key 值
+                                if(alarmStatus!=null) {
+                                    Integer alarmNums=jo1.getInteger("alarmtimes");
+                                    if (alarmNums < 3) {
+                                        jedis.del(redisKey);
+                                        event_operational_status = operational_status;
+                                        event_alert_location = alert_location;
+                                    }else {
+                                        // 记录告警部位 不在时间
+                                        event_operational_status = jo1.getString("operational_status");
+                                        event_alert_location = jo1.getString("alert_location");
+                                    }
+                                }
+                            }
+
                             String tbName = "cms_arithmetic_stable_" + tagName.replace(".", "_");
-                            sqlbuilderback.append(tbName).append("(time,rotation_speed,gearbox_rms_1, gearbox_rms_2, generator_bearing_rms_1,generator_bearing_rms_2, generator_bearing_rms_3, generator_bearing_rms_4, main_bearing_rms_1, main_bearing_rms_2, main_bearing_rms_3, main_bearing_rms_4, operational_status, alert_location, gearbox_mechanical_index, generator_shaft_mechanical_index, main_bearing_mechanical_index, gearbox_health_index,  generator_bearing_health_index, main_bearing_health_index,mechanical_index1,mechanical_index2,mechanical_index3) using cms_arithmetic_stable( tag_name,device_id,standard_code) TAGS('" + tagName + "','" + device_id + "','" + tagName + "')" +
-                                    "values('" + time + "'," + rotation_speed + "," + gearbox_rms_1 + ", " + gearbox_rms_2 + ", " + generator_bearing_rms_1 + "," + generator_bearing_rms_2 + ", " + generator_bearing_rms_3 + ", " + generator_bearing_rms_4 + ", " + main_bearing_rms_1 + "," + main_bearing_rms_2 + ", " + main_bearing_rms_3 + ", " + main_bearing_rms_4 + ", '" + operational_status + "', '" + alert_location + "', " + gearbox_mechanical_index + ", " + generator_shaft_mechanical_index + ", " + main_bearing_mechanical_index + "," + gearbox_health_index + "," + generator_bearing_health_index + ", " + main_bearing_health_index + ", " + mechanical_index1 + ", " + mechanical_index2 + ", " + mechanical_index3 + ")");
+                            sqlbuilderback.append(tbName).append("(time,rotation_speed,gearbox_rms_1, gearbox_rms_2, generator_bearing_rms_1,generator_bearing_rms_2, generator_bearing_rms_3, generator_bearing_rms_4, main_bearing_rms_1, main_bearing_rms_2, main_bearing_rms_3, main_bearing_rms_4, operational_status, alert_location, gearbox_mechanical_index, generator_shaft_mechanical_index, main_bearing_mechanical_index, gearbox_health_index,  generator_bearing_health_index, main_bearing_health_index,mechanical_index1,mechanical_index2,mechanical_index3,event_operational_status,event_alert_location,main_bearing_health_index1,main_bearing_health_index2,main_bearing_health_index3) using cms_arithmetic_stable( tag_name,device_id,standard_code) TAGS('" + tagName + "','" + device_id + "','" + tagName + "')" +
+                                    "values('" + time + "'," + rotation_speed + "," + gearbox_rms_1 + ", " + gearbox_rms_2 + ", " + generator_bearing_rms_1 + "," + generator_bearing_rms_2 + ", " + generator_bearing_rms_3 + ", " + generator_bearing_rms_4 + ", " + main_bearing_rms_1 + "," + main_bearing_rms_2 + ", " + main_bearing_rms_3 + ", " + main_bearing_rms_4 + ", '" + operational_status + "', '" + alert_location + "', " + gearbox_mechanical_index + ", " + generator_shaft_mechanical_index + ", " + main_bearing_mechanical_index + "," + gearbox_health_index + "," + generator_bearing_health_index + ", " + main_bearing_health_index + ", " + mechanical_index1 + ", " + mechanical_index2 + ", " + mechanical_index3 +",'"+event_operational_status+"','"+event_alert_location+"',"+main_bearing_health_index1+ ","+main_bearing_health_index2+ ","+main_bearing_health_index3+ ")");
 
                             System.out.println(statement.execute(sqlbuilderback.toString()));
                         }
@@ -121,11 +190,10 @@ public class CMS2ZnTSDBApplication {
                 if (needSleep) {
                     Thread.sleep(5000);
                 }
+                jedis.close();
             } catch (Exception e) {
-
                 e.printStackTrace();
             }
-
 
         }
 
--- data-web-process/src/test/java/com/ali/zn/data/dataprocess/redis/RedisTest.java
+++ data-web-process/src/test/java/com/ali/zn/data/dataprocess/redis/RedisTest.java
@@ -109,31 +109,34 @@ public class RedisTest {
         try {
             Class.forName("com.taosdata.jdbc.rs.RestfulDriver");
 
-            Connection conn = DriverManager.getConnection("jdbc:TAOS-RS://10.162.201.119:6041/cms_arithmetic_db?user=root&password=taosdata&httpConnectTimeout=600000&httpSocketTimeout=600000&timezone=UTC-8&charset=UTF-8&locale=en_US.UTF-8&httpPoolSize=350");
+            Connection conn = DriverManager.getConnection("jdbc:TAOS-RS://10.162.200.58:6041/cms_arithmetic_db?user=root&password=taosdata&httpConnectTimeout=600000&httpSocketTimeout=600000&timezone=UTC-8&charset=UTF-8&locale=en_US.UTF-8&httpPoolSize=350");
             Statement statement = conn.createStatement();
             StringBuilder sqlbuilder = new StringBuilder("insert into   ");
 
             String stableName = "cms_arithmetic_stable";
             Integer rownum = 1000;
-            File file = new File("E:\\data\\cms\\上电6WM\\arith");
+//            File file = new File("E:\\data\\cms\\上电6WM\\arith");
 //            File file = new File("E:\\data\\cms\\明阳更新0623-2\\明阳更新0623-2");
+            File file = new File("F:\\data\\cms\\json字段0822");
 
 
-            FileWriter fw = new FileWriter("E:\\data\\cms\\明阳更新0623-2/ssmy_arithmetics0623-2.sql");
+//            FileWriter fw = new FileWriter("E:\\data\\cms\\明阳更新0623-2/ssmy_arithmetics0623-2.sql");
             if (file.isDirectory()) {
 
-//                JedisSentinelPool sentinelPool = new JedisSentinelPool(masterName, sentinels, poolConfig,10000,"Zjhc@1234!",14);
-//
-//                Jedis jedis = sentinelPool.getResource();
+                JedisSentinelPool sentinelPool = new JedisSentinelPool(masterName, sentinels, poolConfig,10000,"Zjhc@1234!",14);
+
+
                 JedisPoolConfig config = new JedisPoolConfig();
                 config.setMaxTotal(20);    // 最大连接数
                 config.setMaxIdle(5);      // 最大空闲连接
                 JedisPool pool = new JedisPool(config, "10.162.200.26", 8000, 10000, null, 14);
-                Jedis jedis = pool.getResource();
+
 
                 File[] fileNames = file.listFiles();
 
                 for (File f : fileNames) {
+                    Jedis jedis = pool.getResource();
+//                    Jedis jedis = sentinelPool.getResource();
                     System.out.println(f.getName());
 
                     FileReader fr = new FileReader(f.getAbsoluteFile());
@@ -165,8 +168,8 @@ public class RedisTest {
                             String operational_status = jo.getString("operational_status");
                             String alert_location = jo.getString("alert_location");
 
-                            String event_operational_status = jo.getString("operational_status");
-                            String event_alert_location = jo.getString("alert_location");
+                            String event_operational_status = "健康";
+                            String event_alert_location = "";
 
                             redisJo.put("operational_status", operational_status);
                             redisJo.put("alert_location", alert_location);
@@ -183,44 +186,66 @@ public class RedisTest {
                             Double mechanical_index2 = jo.getDouble("mechanical_index2");
                             Double mechanical_index3 = jo.getDouble("mechanical_index3");
 
+                            //2025年8月25日新增三个字段
+                            Double main_bearing_health_index1 = jo.getDouble("main_bearing_health_index1");
+                            Double main_bearing_health_index2 = jo.getDouble("main_bearing_health_index2");
+                            Double main_bearing_health_index3 = jo.getDouble("main_bearing_health_index3");
 
+                            redisJo.put("alarmtimes",0);
                             String device_id = jo.getString("device_id");
                             String tagName = "5211.CMS." + device_id;
                             String redisKey = redisPrefix + tagName;
                             // 遇到不健康的则更新 状态
+                           String  alarmStatus= jedis.get(redisKey);
+
+
                             if (!operational_status.equals("健康")) {
-                                jedis.set(redisKey, redisJo.toString());
-                            }
-
-
-                            jo.put("tag_name", tagName);
-                            String redisContent = jedis.get(redisKey);
-                            if (redisContent != null) {
-                                JSONObject redisINfo = JSONObject.parseObject(redisContent);
-                                if (!redisINfo.getString("operational_status").equals("健康")) {
-                                    // 更新后续值状态
-                                    event_operational_status = redisINfo.getString("operational_status");
-                                    event_alert_location = redisINfo.getString("alert_location");
+                                if(alarmStatus==null) {
+                                    redisJo.put("alarmtimes",1);
+                                    jedis.set(redisKey,  redisJo.toJSONString());
+                                }else{
+                                    // 更新redis 数字
+                                    JSONObject jo1 = JSONObject.parseObject(alarmStatus);
+                                    Integer alarmNums=jo1.getInteger("alarmtimes")+1;
+                                    redisJo.put("alarmtimes",alarmNums);
+                                    jedis.set(redisKey, redisJo.toString());
+                                    if(alarmNums>=3){
+                                        // 更新后续值状态
+                                        event_operational_status =operational_status;
+                                        event_alert_location = alert_location;
+                                    }
+                                }
+                            }else{
+                                JSONObject jo1 = JSONObject.parseObject(alarmStatus);
+                                // 当不满足 连续三次式需要清空redis 里面的key 值
+                                if(alarmStatus!=null) {
+                                    Integer alarmNums=jo1.getInteger("alarmtimes");
+                                    if (alarmNums < 3) {
+                                        jedis.del(redisKey);
+                                        event_operational_status = operational_status;
+                                        event_alert_location = alert_location;
+                                    }else {
+                                        // 记录告警部位 不在时间
+                                        event_operational_status = jo1.getString("operational_status");
+                                        event_alert_location = jo1.getString("alert_location");
+                                    }
                                 }
                             }
 
                             String tbName = "cms_arithmetic_stable_" + tagName.replace(".", "_");
-                            sqlbuilderback.append(tbName).append("(time,rotation_speed,gearbox_rms_1, gearbox_rms_2, generator_bearing_rms_1,generator_bearing_rms_2, generator_bearing_rms_3, generator_bearing_rms_4, main_bearing_rms_1, main_bearing_rms_2, main_bearing_rms_3, main_bearing_rms_4, operational_status, alert_location, gearbox_mechanical_index, generator_shaft_mechanical_index, main_bearing_mechanical_index, gearbox_health_index,  generator_bearing_health_index, main_bearing_health_index,mechanical_index1,mechanical_index2,mechanical_index3,event_operational_status,event_alert_location) using cms_arithmetic_stable( tag_name,device_id,standard_code) TAGS('" + tagName + "','" + device_id + "','" + tagName + "')" +
-                                    "values('" + time + "'," + rotation_speed + "," + gearbox_rms_1 + ", " + gearbox_rms_2 + ", " + generator_bearing_rms_1 + "," + generator_bearing_rms_2 + ", " + generator_bearing_rms_3 + ", " + generator_bearing_rms_4 + ", " + main_bearing_rms_1 + "," + main_bearing_rms_2 + ", " + main_bearing_rms_3 + ", " + main_bearing_rms_4 + ", '" + operational_status + "', '" + alert_location + "', " + gearbox_mechanical_index + ", " + generator_shaft_mechanical_index + ", " + main_bearing_mechanical_index + "," + gearbox_health_index + "," + generator_bearing_health_index + ", " + main_bearing_health_index + ", " + mechanical_index1 + ", " + mechanical_index2 + ", " + mechanical_index3 + ", '" + operational_status + "', '" + alert_location + "')");
-//                                System.out.println(statement.execute(sqlbuilderback.toString()));
-//                            fw.write(sqlbuilderback.append(";").append("\r\n").toString());
-//                            }
-                            statement.execute(sqlbuilderback.toString());
+                            sqlbuilderback.append(tbName).append("(time,rotation_speed,gearbox_rms_1, gearbox_rms_2, generator_bearing_rms_1,generator_bearing_rms_2, generator_bearing_rms_3, generator_bearing_rms_4, main_bearing_rms_1, main_bearing_rms_2, main_bearing_rms_3, main_bearing_rms_4, operational_status, alert_location, gearbox_mechanical_index, generator_shaft_mechanical_index, main_bearing_mechanical_index, gearbox_health_index,  generator_bearing_health_index, main_bearing_health_index,mechanical_index1,mechanical_index2,mechanical_index3,event_operational_status,event_alert_location,main_bearing_health_index1,main_bearing_health_index2,main_bearing_health_index3) using cms_arithmetic_stable( tag_name,device_id,standard_code) TAGS('" + tagName + "','" + device_id + "','" + tagName + "')" +
+                                    "values('" + time + "'," + rotation_speed + "," + gearbox_rms_1 + ", " + gearbox_rms_2 + ", " + generator_bearing_rms_1 + "," + generator_bearing_rms_2 + ", " + generator_bearing_rms_3 + ", " + generator_bearing_rms_4 + ", " + main_bearing_rms_1 + "," + main_bearing_rms_2 + ", " + main_bearing_rms_3 + ", " + main_bearing_rms_4 + ", '" + operational_status + "', '" + alert_location + "', " + gearbox_mechanical_index + ", " + generator_shaft_mechanical_index + ", " + main_bearing_mechanical_index + "," + gearbox_health_index + "," + generator_bearing_health_index + ", " + main_bearing_health_index + ", " + mechanical_index1 + ", " + mechanical_index2 + ", " + mechanical_index3 +",'"+event_operational_status+"','"+event_alert_location+"',"+main_bearing_health_index1+ ","+main_bearing_health_index2+ ","+main_bearing_health_index3+ ")");
+
+                            System.out.println(statement.execute(sqlbuilderback.toString()));
 
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
                     }
-//                    fw.flush();
+                    jedis.close();
                 }
-//                fw.flush();
             }
-            fw.close();
+//            fw.close();
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
